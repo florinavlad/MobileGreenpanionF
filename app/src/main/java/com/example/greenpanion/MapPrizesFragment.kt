9e9fd3e57a02bac4fdb30329ce3f4c276f8f3783
You Name <you@example.com>
2023-05-18 12:48:03 +0300

    spinner for centers

--- app/src/main/java/com/example/greenpanion/MapPrizesFragment.kt
+++ app/src/main/java/com/example/greenpanion/MapPrizesFragment.kt
@@ -7,15 +7,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 
 class MapPrizesFragment : Fragment() {
 
     private lateinit var etUserLocation: EditText
-    private lateinit var etCenterLocation: EditText
     private lateinit var directionBtn: Button
+    private lateinit var centerSpinner: Spinner
+    private lateinit var centerAdapter: ArrayAdapter<CharSequence>
+    private lateinit var selectedCenter: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,19 +38,59 @@ class MapPrizesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        centerSpinner = view.findViewById(R.id.centers_spinner)
+        centerAdapter = ArrayAdapter.createFromResource(
+            requireActivity(),
+            R.array.array_of_centers,
+            R.layout.spinner_centers_layout
+        )
+        centerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        centerSpinner.adapter = centerAdapter
+
+        centerSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+                selectedCenter = centerSpinner.selectedItem.toString()
+                if (parent?.id == R.id.state_Spinner) {
+
+                    when (selectedCenter) {
+                        "Centre de reciclare" -> centerAdapter = ArrayAdapter.createFromResource(
+                            requireContext(),
+                            R.array.array_of_centers,
+                            R.layout.spinner_layout
+                        )
+
+                        else -> {
+                            // code for handling other cases
+                        }
+                    }
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
         etUserLocation = view.findViewById(R.id.user_location)
-        etCenterLocation = view.findViewById(R.id.center_destination)
         directionBtn = view.findViewById(R.id.direction_Btn)
 
         directionBtn.setOnClickListener() {
             val userLocation = etUserLocation.text.toString()
-            val centerLocation = etCenterLocation.text.toString()
+            val centerSpin = centerSpinner.selectedItem.toString()
 
-            if (userLocation.isEmpty() && centerLocation.isEmpty()) {
-                Toast.makeText(requireContext(), "Locația ta și a centrului trenuie completate", Toast.LENGTH_SHORT).show()
+            if (userLocation.isEmpty()) {
+                Toast.makeText(requireContext(), "Locația ta trebuie completată", Toast.LENGTH_SHORT).show()
+            }
+            else if (selectedCenter == "Centre de reciclare") {
+            Toast.makeText(requireContext(), "Selectează centrul dorit", Toast.LENGTH_SHORT).show()
             }
             else {
-                val uri = Uri.parse("https://www.google.com/maps/dir/$userLocation/$centerLocation")
+                val uri = Uri.parse("https://www.google.com/maps/dir/$userLocation/$$centerSpin")
                 val intent = Intent(Intent.ACTION_VIEW, uri)
                 intent.`package` = "com.google.android.apps.maps"
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
